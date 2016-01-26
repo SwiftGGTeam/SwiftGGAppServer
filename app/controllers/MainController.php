cb4c5233492c45c740d1018d7d81df15d689fc4b
chenmingbiao <mbillchan@gmail.com>
2016-01-26 22:38:12 +0800

    修改Main控制器

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,12 +4,12 @@
 // use \Controller;
 
 // require_once (SWIFTGGAPPLIB_PATH.'libs/ILog.php');
-require_once (APPLIB_PATH.'libs/CatchArticle.php');
+// require_once (APPLIB_PATH.'libs/CatchArticle.php');
 
 class MainController extends Controller {
 
     public function index() {       
-
+    	echo 'Hello SwiftGG!';
     }
 
 }
